406737d5ab03b62e0c10e4678a461181a97db9af
te <jorge.eff.96@gmail.com>
2021-01-03 22:28:28 -0300

    feasible func

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -1,4 +1,15 @@
 from tree import *
 
+def feasible(restrictions):
+    # restrictions = []
+    valids = 0
+    for r in restrictions:
+        if r:
+            valids += 1 
+    return valids == len(restrictions)
+
+def simplex():
+    return True
+
 def branch_and_bound():
     return True
